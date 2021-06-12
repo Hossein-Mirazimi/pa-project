0671864ff762cdc162857f17090c84133bb98b8f
Hossein Mirazimi <hossein.ma1832@gmail.com>
2021-06-13 04:28:53 +0430

    User login and tokens added to project

--- server/model/user.js
+++ server/model/user.js
@@ -1,7 +1,14 @@
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const _ = require('lodash');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const { mongoose } = require('./../db/mongoose');
-
+const tokenOptions = {
+  type: String,
+  required: true,
+};
 let UserSchema = new mongoose.Schema({
   fullName: {
     type: String,
@@ -25,7 +32,82 @@ let UserSchema = new mongoose.Schema({
     minlength: 6,
     required: true,
   },
+  tokens: [
+    {
+      access: tokenOptions,
+      token: tokenOptions,
+    },
+  ],
 });
+
+UserSchema.methods.toJSON = function () {
+  let user = this;
+
+  let userObject = user.toObject();
+
+  return _.pick(userObject, ['fullName', 'email', '_id']);
+};
+
+UserSchema.methods.generateAuthToken = function () {
+  let user = this;
+  let access = 'auth';
+
+  let token = jwt
+    .sign(
+      {
+        _id: user._id.toHexString(),
+        access,
+      },
+      config.get('JWT_SECRET')
+    )
+    .toString();
+
+  user.tokens.push({
+    access,
+    token,
+  });
+
+  return user.save().then(() => {
+    return token;
+  });
+};
+
+UserSchema.pre('save', function (next) {
+  let user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+UserSchema.statics.findByCredentials = function (email, password) {
+  let User = this;
+
+  return User.findOne({
+    email,
+  }).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+};
 
 let User = mongoose.model('User', UserSchema);
 
