9940165c70dc0c5d9f69c629f0d598bed0386e48
Hossein Mirazimi <hossein.ma1832@gmail.com>
2021-06-13 02:19:31 +0430

    First commit - Config module added to the project

---
+++ server/server.js
@@ -0,0 +1,8 @@
+//---Config
+process.env.NODE_CONFIG_DIR = __dirname + '/config';
+
+const config = require('config');
+
+console.log(`*** ${String(config.get('level')).toUpperCase()} ***`);
+console.log(config.get('MONGOURI'));
+console.log(config.get('PORT'));
